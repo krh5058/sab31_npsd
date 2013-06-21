7b7f7a105331ce2519ea2ef29ca46f4ca6954817
krh5058 <ken.r.hwang@gmail.com>
2013-06-21 16:16:55 -0400

    062113
    See README

--- bin/pres.m
+++ bin/pres.m
@@ -3,17 +3,15 @@ classdef pres < handle
     % See dated ReadMe file
     
     properties
-        movie
+        img
         txt
         fix_color
         misc
-        keys
         lh
     end
     
     properties (SetObservable)
         temp_t
-        abort = 0;
     end
     
     methods
@@ -24,24 +22,18 @@ classdef pres < handle
             misc.fix1 = @(monitor,color)(Screen('DrawLine',monitor.w,color,monitor.center_W-20,monitor.center_H,monitor.center_W+20,monitor.center_H,7));
             misc.fix2 = @(monitor,color)(Screen('DrawLine',monitor.w,color,monitor.center_W,monitor.center_H-20,monitor.center_W,monitor.center_H+20,7));
             misc.text = @(monitor,txt)(DrawFormattedText(monitor.w,txt,'center','center',monitor.white));
-            
-            % Keys
-            fprintf('pres.m (pres): Defining key press identifiers...\n');
-            KbName('UnifyKeyNames');
-            keys.esckey = KbName('Escape');
-            keys.spacekey = KbName('SPACE');
-            keys.tkey = KbName('t');
+            misc.mktex = @(monitor,img)(Screen('MakeTexture',monitor.w,img));
+            misc.drwtex = @(monitor,tex)(Screen('DrawTexture',monitor.w,tex));
             
             % Listeners
             fprintf('pres.m (pres): Defining listener function handles...\n');
             lh.lh1 = addlistener(src,'fix',@(src,evt)dispfix(obj,src,evt));
-            lh.lh2 = addlistener(src,'playback',@(src,evt)videoplayback(obj,src,evt));
+            lh.lh2 = addlistener(src,'showimg',@(src,evt)dispimg(obj,src,evt));
             lh.lh3 = addlistener(src,'txt',@(src,evt)disptxt(obj,src,evt));
             lh.lh4 = addlistener(src,'dat','PostSet',@(src,evt)propset(obj,src,evt));
             
             fprintf('pres.m (pres): Storing object properties...\n');
             obj.misc = misc;
-            obj.keys = keys;
             obj.lh = lh;
             
             fprintf('pres.m (pres): Success!\n');
@@ -53,8 +45,11 @@ classdef pres < handle
             obj.temp_t = Screen('Flip',src.monitor.w);
         end
         
-        function showimg(obj,src,evt) % Corresponding to lh2
-            
+        function dispimg(obj,src,evt) % Corresponding to lh2
+            tex = obj.misc.mktex(src.monitor,obj.img);
+            obj.misc.drwtex(src.monitor,tex);
+            obj.temp_t = Screen('Flip',src.monitor.w);
+            Screen('Close',tex);
         end
         
         function disptxt(obj,src,evt) % Corresponding to lh3
